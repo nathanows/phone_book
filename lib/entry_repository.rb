233833ba93300d3b37a3d913f5b87f928caa94ff
Nathan Owsiany <nowsiany@gmail.com>
2014-11-05 14:23:22 -0700

    started entry repository

---
+++ lib/entry_repository.rb
@@ -0,0 +1,10 @@
+class EntryRepository
+  def initialize(entries)
+    
+  end
+
+  def find_by_last_name(last_name)
+    []
+  end
+
+end
---
+++ test/entry_repository_test.rb
@@ -0,0 +1,25 @@
+gem 'minitest', '~> 5.4'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/entry_repository'
+
+class EntryRepositoryTest < Minitest::Test
+  def test_retrieve_by_last_name
+    entries = [
+      { first_name: 'Alice', last_name: 'Smith', phone_number: '111.111.1111'},
+      { first_name: 'Bob', last_name: 'Smith', phone_number: '222.222.2222'},
+      { first_name: 'Cindy', last_name: 'Smith', phone_number: '333.333.3333'}
+    ]
+
+    repository = EntryRepository.new(entries)
+    entries = repository.find_by_last_name("Smith").sort_by { |e| e.first_name }
+    assert_equal 2, entries.length
+
+    alice, bob = entries
+
+    assert_equal "Alice Smith", alice.name
+    assert_equal "111.111.1111", alice.phone_number
+    assert_equal "Bob Smith", bob.name
+    assert_equal "222.222.2222", bob.phone_number
+  end
+end
