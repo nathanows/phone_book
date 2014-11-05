ab5863aeb844e1284f6d37cdcea298512f733ce7
Nathan Owsiany <nowsiany@gmail.com>
2014-11-05 13:30:15 -0700

    wrote first integration test

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -4,4 +4,20 @@ require 'minitest/pride'
 require '../lib/phone_book'
 
 class IntegrationTest < MiniTest::Test
+  def test_lookup_by_last_name
+    phone_book = PhoneBook.new
+    entries = phone_book.lookup('Parker').sort_by { |e| e.first_name }
+
+    assert 3, entries.length
+    e1, e2, e3 = entries
+
+    assert_equal "Agnes Parker", e1.name
+    assert_equal "758.942.6890", e1.phone_number
+
+    assert_equal "Craig Parker", e2.name
+    assert_equal "716-133-3210", e2.phone_number
+
+    assert_equal "Mohamed Parker", e3.name
+    assert_equal "701-655-6889", e3.phone_number
+  end
 end
