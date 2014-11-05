8c1dab6a14b417c61c26cc493c2b2420515fea85
Emily Berkeley <eberkeley@gmail.com>
2014-11-05 14:10:28 -0700

    Updated entry class

--- lib/entry.rb
+++ lib/entry.rb
@@ -2,5 +2,12 @@ class Entry
   attr_reader :first_name, :last_name, :phone_number
 
   def initialize(data)
+    @first_name = data[:first_name]
+    @last_name = data[:last_name]
+    @phone_number = data[:phone_number]
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
   end
 end
